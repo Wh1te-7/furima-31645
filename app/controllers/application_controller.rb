782fb8b8e013f77b093da324dcab53e0451a195e
Wh1te-7 <ohas7cancer@icloud.com>
2020-11-19 18:48:33 +0900

    ユーザーの管理機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,7 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :authenticate_user!
 
   private
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '2222'
-    end
-  end
+
 end
