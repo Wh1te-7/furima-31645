f96fe8f106fff651f42469b9c8e25cb80364a77d
Wh1te-7 <ohas7cancer@icloud.com>
2020-11-24 21:18:07 +0900

    商品出品機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,34 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :customer
+  has_many :comments
+  has_one_attached :image
   
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery
+  belongs_to_active_hash :area
+  belongs_to_active_hash :day
+
+  with_options presence: true do
+    validates :title
+    validates :concept
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :status_id
+      validates :delivery_id
+      validates :area_id
+      validates :days_id
+    end
+    validates :price, 
+    numericality: { 
+      greater_than_or_equal_to: 300, 
+      less_than_or_equal_to: 9999999
+    }, 
+    format: {
+      with: /\A[-]?[0-9]+(\.[0-9]+)?\z/
+    }
+    validates :image
+  end
 end
--- db/migrate/20201123052056_create_items.rb
+++ db/migrate/20201123052056_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :title,        null:false
+      t.text :concept,        null:false
+      t.integer :category_id, null:false
+      t.integer :status_id,   null:false
+      t.references :user,     foreign_key: true
+      t.integer :delivery_id, null:false
+      t.integer :area_id,     null:false
+      t.integer :days_id,     null:false
+      t.integer :price,       null:false
       t.timestamps
     end
   end
