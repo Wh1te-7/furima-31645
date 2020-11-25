580814f91372286de4c0917fc1376f47a003c5bd
Wh1te-7 <ohas7cancer@icloud.com>
2020-11-25 15:12:09 +0900

    単体テストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,97 @@
 require 'rails_helper'
+describe Item do
+  before do
+    @item = FactoryBot.build(:item)
+  end
 
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '商品の新規登録' do
+    context '新規登録がうまくいくとき' do
+      it "title,concept,category_id,status_id,delivery_id,area_id,days_id,price,が存在すれば登録できる" do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '新規登録がうまくいかないとき' do
+      it "titleが空だと登録できない" do
+        @item.title = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Title can't be blank")
+      end
+      it "conceptが空だと登録できない" do
+        @item.concept = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Concept can't be blank")
+      end
+      it "category_idが空だと登録できない" do
+        @item.category_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank", "Category is not a number")
+      end
+      it "category_idのidが1の場合登録できない" do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+      it "status_idが空だと登録できない" do
+        @item.status_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status can't be blank", "Status is not a number")
+      end
+      it "status_idのidが1の場合登録できない" do
+        @item.status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
+      end
+      it "delivery_idが空だと登録できない" do
+        @item.delivery_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery can't be blank", "Delivery is not a number")
+      end
+      it "delivery_idのidが1の場合登録できない" do
+        @item.delivery_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery must be other than 1")
+      end
+      it "area_idが空だと登録できない" do
+        @item.area_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area can't be blank", "Area is not a number")
+      end
+      it "area_idのidが1の場合登録できない" do
+        @item.area_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area must be other than 1")
+      end
+      it "days_idが空だと登録できない" do
+        @item.days_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days can't be blank", "Days is not a number")
+      end
+      it "days_idのidが1の場合登録できない" do
+        @item.days_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days must be other than 1")
+      end
+      it "priceが空だと登録できない" do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it "priceが半角数字以外の場合登録できない" do
+        @item.price = "AＢあ亜ア０"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it "priceが300未満だと登録できない" do
+        @item.price = 290
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it "priceが10000000以上だと登録できない" do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+    end
+  end
 end
