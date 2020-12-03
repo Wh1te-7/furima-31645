a8b470e167124396a9a91ff5fab43409e36f602e
Wh1te-7 <ohas7cancer@icloud.com>
2020-12-03 17:48:49 +0900

    単体テストコード

---
+++ spec/factories/user_donations.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user_donation do
+    postal {"344-0087"}
+    area_id {2}
+    home {'埼玉県さいたま市浦和区'}
+    home_number{'東岸町1-6-5'}
+    number {'09098607564'}
+    token {"tok_abcdefghijk00000000000000000"}
+  end
+end
